f7bd9708cefd89b7676de22790a8847df8c9bd40
cossonleo <cossonleo@foxmail.com>
2020-07-13 21:45:29 +0800

    定制references回调

--- lua/cossonleo/lsp.lua
+++ lua/cossonleo/lsp.lua
@@ -1,4 +1,21 @@
 
 local M = {}
 
+M.references = function()
+  local params = vim.lsp.util.make_position_params()
+  params.context = {
+    includeDeclaration = true;
+  }
+  params[vim.type_idx] = vim.types.dictionary
+
+  return vim.lsp.buf_request(0, 'textDocument/references', params, function(_, _, result)
+	  if not result then return end
+	  vim.lsp.util.set_qflist(vim.lsp.util.locations_to_items(result))
+	  local wvv = vim.fn.winsaveview()
+	  vim.api.nvim_command("copen")
+	  vim.api.nvim_command("wincmd p")
+	  vim.fn.winrestview(wvv)
+  end)
+end
+
 return M
