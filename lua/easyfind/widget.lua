8a50ad14843b6112f880eced6bf25d64d3f662b7
cossonleo <cossonleo@foxmail.com>
2020-10-05 18:22:03 +0800

    upd

--- lua/easyfind/widget.lua
+++ lua/easyfind/widget.lua
@@ -1,43 +1,88 @@
 
 local a = vim.api
-local M
+local M = {}
 
 local pbuf = 0
-local mbuf = 0
+local match_str = ""
+local match_cache = {}
 local list_data = {}
-local cur_item = 0
+
+local main_buf = {
+	id = 0,
+	start = 0,
+	cur = 0,
+}
+
+local config = {
+	m_height = 0
+	m_width = 0
+	p_height = 0
+	p_wdith = 0
+}
 
 M.item = {
-	line_show = function(buf, line, match) end
+	line = function(match) end
 	preview = function(buf) end
 	do = function() end
 }
 
-local function create_mbuf()
-	if mbuf ~= 0 then return end
-	mbuf = a.nvim_create_buf(false, false)
+function main_buf.reset()
+	main_buf.cur = 1
+	main_buf.start = 1
+
+	if main_buf.id ~= 0 then return end
+	main_buf.id = a.nvim_create_buf(false, false)
 	-- set hotkey
 end
 
-local function reset_mbuf()
-
+local function hi_cur_line()
 end
 
-local function show()
-	reset_mbuf()
-	cur_item = 1
+local function match_items()
+	local new_match = {}
+	local check = function(item)
+		local display = item:line(match_str)
+		if display then
+			table.insert(new_match, {item = item, display = display})
+		end
+	end
+
+	if #match_cache > 0 then
+		for _, cache in ipairs(match_cache) do
+			check(cache.item)
+		end
+	else
+		for _, item in ipairs(list_data) do
+			check(item)
+		end
+	end
+
+	match_cache = new_match
+end
+
+local function update()
 	local line = 2
-	for _, item in ipairs(list_data) do
-		if item.line_show(mbuf, line, "") then
-			line + 1
+	local new_match = {}
+	for _, item in ipairs(match_list) do
+		local line_info = item:line(match_str)
+		if line_info then
+			table.insert(new_match, item)
 		end
 	end
 
 	-- disyplay mwin
 
-	if list_data[cur_item].preview(pbuf) then
+	if list_data[cur_item]:preview(pbuf) then
 		--  display preview
 	end
+end
+
+local function show()
+	main_buf.reset()
+	match_str = ""
+	match_cache = {}
+	match_items()
+	M.update()
 end
 
 function M.show(data)
@@ -47,5 +92,19 @@ function M.show(data)
 	show()
 end
 
+function M.do()
+end
+
+function M.move_next()
+	cur_item = cur_item + 1
+	if cur_item > #match_list then cur_item = 1 end
+	M.update()
+end
+
+function M.move_prev()
+	if cur_item <= 1 then cur_item = #match_list end
+	cur_item = cur_item - 1
+	M.update()
+end
 
 return M
