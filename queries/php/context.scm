0fae03e539d40ce1f01cc9db33b1849fe62ca985
cossonleo <cossonleo@foxmail.com>
2020-12-03 17:22:33 +0800

    update

---
+++ queries/php/context.scm
@@ -0,0 +1,16 @@
+; Scopes
+;-------
+[
+	(class_declaration)
+	(method_declaration)
+	(function_definition)
+] @scope
+
+(class_declaration
+  name: (name) @context_name)
+
+(method_declaration
+  name: (name) @context_name)
+
+(function_definition
+  name: (name) @context_name)
---
+++ queries/python/context.scm
@@ -0,0 +1,10 @@
+[
+	(function_definition)
+	(class_definition)
+] @scope
+
+(function_definition
+  name: (identifier) @context_name)
+
+(class_definition
+  name: (identifier) @context_name)
