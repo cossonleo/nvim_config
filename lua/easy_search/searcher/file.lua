e7baba4524c2f2b56f2eb88acd4ea50386f0f685
cossonleo <cossonleo@foxmail.com>
2021-03-01 00:11:44 +0800

    增加rg搜索文件

--- lua/easy_search/searcher/file.lua
+++ lua/easy_search/searcher/file.lua
@@ -35,17 +35,50 @@ function file_item:new(name, win)
 	return item
 end
 
+local function generate_items(files)
+	local win = vim.api.nvim_get_current_win()
+	local items = {}
+	for _, f in ipairs(files) do
+		local item = file_item:new(f, win)
+		table.insert(items, item)
+	end
+	require("easy_search.ui").new(items)
+end
+
+local function collect_by_rg()
+	local pwd = vim.fn.getcwd(-1, 0)
+	local cmd = "rg --files " .. pwd
+	local t = io.popen(cmd)
+	local str = t:read("*a")
+	if #str == 0 then return end
+	local files = {}
+	local start = #pwd + 2
+	for p in vim.gsplit(str, "\n") do
+		local f = p:sub(start)
+		if #f > 0 then
+			table.insert(files, f)
+		end
+	end
+	generate_items(files)
+end
+
+local function collect_by_builtin()
+	local pwd = vim.fn.getcwd(-1, 0)
+	local paths = require("share_sugar").scan_dir_rec(pwd, true) 
+	local files = {}
+	for _, file in ipairs(paths) do
+		table.insert(files, file:sub(#pwd + 2))
+	end
+			local sub = p:sub(#pwd + 2)
+	generate_items(files)
+end
+
 return {
 	search = function() 
-		local win = vim.api.nvim_get_current_win()
-		local pwd = vim.fn.getcwd(-1, 0)
-		local vec = require("share_sugar").scan_dir_rec(pwd, true) 
-		local items = {}
-		for _, p in ipairs(vec) do
-			local sub = p:sub(#pwd + 2)
-			local item = file_item:new(sub, win)
-			table.insert(items, item)
+		if vim.fn.executable("rg") == 1 then
+			collect_by_rg()
+		else
+			collect_by_builtin()
 		end
-		require("easy_search.ui").new(items)
 	end
 }
