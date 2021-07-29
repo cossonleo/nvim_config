20c5b774d8bc563c964be94e5e1e48369c771cb3
cossonleo <cossonleo@foxmail.com>
2021-07-29 10:15:13 +0800

    upd

--- lua/gutil/file.lua
+++ lua/gutil/file.lua
@@ -78,27 +78,43 @@ local function _scan_dir_rec(path, ignore)
 	local uv = vim.loop
 
 	if not path or #path == "" then return {} end
-
-	local dir = uv.fs_opendir(path, nil, 1000)
-	if not dir then return {} end
-
 	local vec = {}
 	local sub_dir = {}
 
-	repeat
-		local entries = uv.fs_readdir(dir) or {}
-		for _, entry in ipairs(entries) do
-			local p = path .. "/" .. entry.name
-			if not ignore or not nvim.is_ignore_path(p) then
-				if entry.type == "file" then
-					table.insert(vec, p)
-				elseif entry.type == "directory" then
-					table.insert(sub_dir, p)
-				end
+	local dir_iter = uv.fs_scandir(path)
+	if not dir_iter then return vec end
+	while (true) do
+		local entry, type = uv.fs_scandir_next(dir_iter)
+		if not entry then break end
+		local p = path .. "/" .. entry
+		if not ignore or not nvim.is_ignore_path(p) then
+			if type == "file" then
+				table.insert(vec, p)
+			elseif type == "directory" then
+				table.insert(sub_dir, p)
 			end
 		end
-	until (#entries == 0)
-	uv.fs_closedir(dir)
+	end
+
+	-- 下面过程会发生coredump， 原因 调用closedir报duoble free错误
+	--local dir = uv.fs_opendir(path, nil, 1000)
+	--if not dir then return vec end
+
+	--while (true) do
+	--	local entries = dir:readdir()
+	--	if not entries then break end
+	--	for _, entry in ipairs(entries) do
+	--		local p = path .. "/" .. entry.name
+	--		if not ignore or not nvim.is_ignore_path(p) then
+	--			if entry.type == "file" then
+	--				table.insert(vec, p)
+	--			elseif entry.type == "directory" then
+	--				table.insert(sub_dir, p)
+	--			end
+	--		end
+	--	end
+	--end
+	--dir:closedir()
 
 	for _, sd in ipairs(sub_dir) do
 		local sub_f = _scan_dir_rec(sd, ignore)
